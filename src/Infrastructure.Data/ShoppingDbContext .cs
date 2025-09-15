ce61f27074d30acb02fe01e5efff662c75f0cb51
Serhii Molozhenko <molozhenko2011@gmail.com>
2025-09-15 15:55:12 +0300

    add db context

---
+++ src/Infrastructure.Data/ShoppingDbContext .cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ShoppingDbContext : DbContext
+{
+    public ShoppingDbContext(DbContextOptions<ShoppingDbContext> options) : base(options) { }
+
+    public DbSet<ShoppingList> ShoppingLists { get; set; } = null!;
+    public DbSet<Item> Items { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShoppingList>()
+            .HasMany(l => l.Items)
+            .WithOne(i => i.ShoppingList)
+            .HasForeignKey(i => i.ShoppingListId);
+
+        modelBuilder.Entity<Item>()
+            .Property(i => i.Quantity)
+            .HasDefaultValue(1);
+    }
+}
